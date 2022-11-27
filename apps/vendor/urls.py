74bcea9a3210d42d69fadd9baa617e0da13888d2
Vsirotkin <vsirotkin15@gmail.com>
2022-11-27 15:36:33 +0300

    setup signup for a vendor

--- apps/vendor/urls.py
+++ apps/vendor/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from apps.vendor import views
 
 app_name = 'vendor'
-urlpatterns = []
+urlpatterns = [
+    path('become_vendor/', views.become_vendor, name='become-vendor'),
+]
--- apps/vendor/views.py
+++ apps/vendor/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from apps.vendor.models import Vendor
 
 
+def become_vendor(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            vendor = Vendor.objects.create(name=user.username, created_by=user)
+            return redirect('core:frontpage')
+    else:
+        form = UserCreationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'vendor/become_vendor.html', context)
