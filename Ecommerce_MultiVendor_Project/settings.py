ff26fe9fe5134dc19eb57f0ec9a3ba62604d4f6d
Vsirotkin <vsirotkin15@gmail.com>
2022-11-27 14:38:10 +0300

    created vendor app

--- Ecommerce_MultiVendor_Project/settings.py
+++ Ecommerce_MultiVendor_Project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.core.apps.CoreConfig',
+    'apps.vendor.apps.VendorConfig',
 ]
 
 MIDDLEWARE = [
--- Ecommerce_MultiVendor_Project/urls.py
+++ Ecommerce_MultiVendor_Project/urls.py
@@ -10,7 +10,7 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('apps.core.urls', namespace='core')),
+    path('vendor/', include('apps.vendor.urls', namespace='vendor')),
     path('admin/', admin.site.urls),
 ]
