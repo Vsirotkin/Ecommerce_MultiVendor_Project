e5995af8f97dcf5b3505dcb28fd05232ae5f93fd
Vsirotkin <vsirotkin15@gmail.com>
2022-11-28 14:32:46 +0300

    created models: Category, Product
    in Product - img and thumbnail, if thumb is not available, it will be created by Product Model

--- Ecommerce_MultiVendor_Project/settings.py
+++ Ecommerce_MultiVendor_Project/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # own
     'apps.core.apps.CoreConfig',
     'apps.vendor.apps.VendorConfig',
+    'apps.product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +124,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static/'
 ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Ecommerce_MultiVendor_Project/urls.py
+++ Ecommerce_MultiVendor_Project/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('apps.core.urls', namespace='core')),
     path('vendor/', include('apps.vendor.urls', namespace='vendor')),
+    path('product/', include('apps.product.urls', namespace='product')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
