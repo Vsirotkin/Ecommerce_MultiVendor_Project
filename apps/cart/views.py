61a6edbe97937ee6bcf8edea8d86be271aedc79f
vik <vsirotkin15@gmail.com>
2022-12-09 10:24:18 +0300

    added cart update functionality, started new app - order

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -5,10 +5,20 @@ from apps.cart.cart import Cart
 
 def cart_detail(request):
     cart = Cart(request)
+
     remove_from_cart = request.GET.get('remove_from_cart', '')
+    change_quantity = request.GET.get('change_quantity', '')
+    quantity = request.GET.get('quantity', 0)
+
     if remove_from_cart:
         cart.remove(remove_from_cart)
 
         return redirect('cart:cart_detail')
+
+    if change_quantity:
+        cart.add(change_quantity, quantity, True)
+
+        return redirect('cart:cart_detail')
+
     context = {}
     return render(request, 'cart/cart_detail.html', context)
